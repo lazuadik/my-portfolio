3bc110ee5d5ff62b6aca9c0b94c1947b3934692d
lazuardik <andhikarafilazuardi@gmail.com>
2025-11-25 13:26:13 +0700

    fixing pharagraph career journey

--- src/components/CareerJourney.jsx
+++ src/components/CareerJourney.jsx
@@ -8,8 +8,8 @@ function DataJourney() {
             <div className ="container-data-journey-background">
                 <div className="container-data-journey-text">
                     <h2>How about my career journey?📊</h2>
-                    <p>I’ve always loved math and design. In my first two years of university, I was active in UI/UX—joining competitions, working on freelance projects, and tutoring. In my third year.</p>
-                    <p>I shifted to data science, starting with small projects, campus work, and Bangkit Academy (Machine Learning track).</p>
+                    <p>I’ve always loved math and design. In my first two years of university, I was active in UI/UX—joining competitions, working on freelance projects, and tutoring.</p>
+                    <p>In my third year, I shifted to data science, starting with small projects, campus work, and Bangkit Academy (Machine Learning track).</p>
                     <p>Since then, I’ve grown through hands-on experience, including internships as a Data Analyst at Telkom Indonesia and Kalbe Nutritionals. I'm excited to keep exploring the world of data with even more passion.</p>
                 </div>
                 <div className="container-data-journey-image">
